dc9ff3208e1b586753a471d1484f631f4ac1b9d3
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2024-10-30 19:40:32 -0700

    feat: implement access control on search query

--- apps/api/src/graphql/resolvers/search.ts
+++ apps/api/src/graphql/resolvers/search.ts
@@ -1,11 +1,17 @@
 import type { GraphQLContext } from "$graphql/graphql-context";
+import { accessController } from "$middleware";
 import { searchQuerySchema } from "$schema";
 import { CoursesService, InstructorsService, SearchService } from "$services";
 
 export const searchResolvers = {
   Query: {
-    search: async (_: unknown, args: { query: unknown }, { db }: GraphQLContext) => {
-      const service = new SearchService(db, new CoursesService(db), new InstructorsService(db));
+    search: async (_: unknown, args: { query: unknown }, ctx: GraphQLContext) => {
+      await accessController("FUZZY_SEARCH")(ctx.honoContext, async () => {});
+      const service = new SearchService(
+        ctx.db,
+        new CoursesService(ctx.db),
+        new InstructorsService(ctx.db),
+      );
       return await service.doSearch(searchQuerySchema.parse(args.query));
     },
   },
