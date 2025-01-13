c987b06f2ab88625c54ac0418c46b68138a1861f
Andrew Wang <awwang3@uci.edu>
2025-01-13 10:41:24 -0800

    fix: key-manager ui

--- apps/key-manager/src/components/layout/Header.tsx
+++ apps/key-manager/src/components/layout/Header.tsx
@@ -1,12 +1,11 @@
-import { auth, signOut } from "@/auth";
+import { auth } from "@/auth";
+import SignOut from "@/components/auth/SignOut";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { LogOutIcon } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 
 const Header = async () => {
@@ -23,29 +22,14 @@ const Header = async () => {
             {session.user.isAdmin && (
               <p className={"text-destructive text-xl select-none"}>ADMIN</p>
             )}
-            <DropdownMenuTrigger className={"focus:outline-none"}>
-              {session.user.image && session.user.email && (
-                <Image
-                  className={"rounded-full"}
-                  width={40}
-                  height={40}
-                  src={session.user.image}
-                  alt={session.user.email}
-                />
-              )}
+            <DropdownMenuTrigger asChild>
+              <Avatar>
+                <AvatarImage src={session.user.image ?? ""} alt={"avatar"} />
+              </Avatar>
             </DropdownMenuTrigger>
           </div>
           <DropdownMenuContent align={"end"}>
-            <DropdownMenuItem
-              onClick={async () => {
-                "use server";
-                await signOut();
-              }}
-              className={"cursor-pointer"}
-            >
-              <LogOutIcon />
-              <span>Log Out</span>
-            </DropdownMenuItem>
+            <SignOut />
           </DropdownMenuContent>
         </DropdownMenu>
       )}
