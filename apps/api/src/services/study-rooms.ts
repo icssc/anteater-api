b0f918b7e78412759d20f0547ef5b323a701e40b
Dante Dam <laggycomputer@yahoo.com>
2025-01-13 22:50:41 -0800

    feat(study-rooms): slots materialized view

--- apps/api/src/services/study-rooms.ts
+++ apps/api/src/services/study-rooms.ts
@@ -1,8 +1,8 @@
 import type { studyRoomSchema, studyRoomsQuerySchema } from "$schema";
 import type { z } from "@hono/zod-openapi";
 import type { database } from "@packages/db";
-import { and, eq, gte, lte, sql } from "@packages/db/drizzle";
-import { studyRoom, studyRoomSlot } from "@packages/db/schema";
+import { and, eq, gte, lte } from "@packages/db/drizzle";
+import { studyRoom, studyRoomView } from "@packages/db/schema";
 
 type StudyRoomsServiceInput = z.infer<typeof studyRoomsQuerySchema>;
 
@@ -23,24 +23,8 @@ export class StudyRoomsService {
       conditions.push(eq(studyRoom.techEnhanced, input.isTechEnhanced));
 
     return this.db
-      .select({
-        id: studyRoom.id,
-        name: studyRoom.name,
-        capacity: studyRoom.capacity,
-        location: studyRoom.location,
-        description: studyRoom.description,
-        directions: studyRoom.directions,
-        techEnhanced: studyRoom.techEnhanced,
-        slots: sql`ARRAY_AGG(JSONB_BUILD_OBJECT(
-          'studyRoomId', ${studyRoomSlot.studyRoomId},
-          'start', to_json(${studyRoomSlot.start} AT TIME ZONE 'America/Los_Angeles'),
-          'end', to_json(${studyRoomSlot.end} AT TIME ZONE 'America/Los_Angeles'),
-          'isAvailable', ${studyRoomSlot.isAvailable}
-        ))`.as("slots"),
-      })
-      .from(studyRoom)
-      .leftJoin(studyRoomSlot, eq(studyRoom.id, studyRoomSlot.studyRoomId))
-      .groupBy(studyRoom.id)
+      .select()
+      .from(studyRoomView)
       .where(conditions.length ? and(...conditions) : undefined);
   }
 }
