18e727899dcbc0381695005ab430567ca51337de
Eric Tao <etao9039@gmail.com>
2025-05-10 01:05:12 -0700

    refactor: update library traffic schema to use Int for ID and enhance resolver logic

--- apps/api/src/graphql/resolvers/library-traffic.ts
+++ apps/api/src/graphql/resolvers/library-traffic.ts
@@ -1,12 +1,14 @@
 import type { GraphQLContext } from "$graphql/graphql-context";
-import { libraryTrafficQuerySchema } from "$schema";
+import { libraryTrafficQuerySchema, libraryTrafficSchema } from "$schema";
 import { LibraryTrafficService } from "$services";
 
 export const libraryTrafficResolvers = {
   Query: {
     libraryTraffic: async (_: unknown, args: { query: unknown }, { db }: GraphQLContext) => {
       const service = new LibraryTrafficService(db);
-      return await service.getLibraryTraffic(libraryTrafficQuerySchema.parse(args.query));
+      const input = libraryTrafficQuerySchema.parse(args.query);
+      const rows = await service.getLibraryTraffic(input);
+      return libraryTrafficSchema.parse(rows);
     },
   },
 };
