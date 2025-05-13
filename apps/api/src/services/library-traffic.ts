6c015e753535fb126eb970ddddec9bd3a1ba7161
Eric Tao <etao9039@gmail.com>
2025-05-13 13:19:14 -0700

    refactor: update locationName example and fix getLibraryTraffic method for parameter handling

--- apps/api/src/services/library-traffic.ts
+++ apps/api/src/services/library-traffic.ts
@@ -1,6 +1,6 @@
 import type { libraryTrafficQuerySchema } from "$schema";
 import type { database } from "@packages/db";
-import { eq } from "@packages/db/drizzle";
+import { and, eq } from "@packages/db/drizzle";
 import { libraryTraffic } from "@packages/db/schema";
 import type { z } from "zod";
 
@@ -11,6 +11,16 @@ export class LibraryTrafficService {
   constructor(private readonly db: ReturnType<typeof database>) {}
 
   getLibraryTraffic(input: LibraryTrafficServiceInput): Promise<LibraryTrafficServiceOutput[]> {
+    const queryParams = [];
+
+    if (input.libraryName) {
+      queryParams.push(eq(libraryTraffic.libraryName, input.libraryName));
+    }
+
+    if (input.locationName) {
+      queryParams.push(eq(libraryTraffic.locationName, input.locationName));
+    }
+
     return this.db
       .select({
         id: libraryTraffic.id,
@@ -21,6 +31,6 @@ export class LibraryTrafficService {
         timestamp: libraryTraffic.timestamp,
       })
       .from(libraryTraffic)
-      .where(input.locationName ? eq(libraryTraffic.locationName, input.locationName) : undefined);
+      .where(queryParams.length ? and(...queryParams) : undefined);
   }
 }
