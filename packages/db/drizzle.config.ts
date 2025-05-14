80bd8e3dbebdb041109078fa81a91640641f952f
Hyunsu Lim <hyunsul2@uci.edu>
2025-05-13 20:25:34 -0700

    fix: manual rollback

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -2,15 +2,8 @@ import "dotenv/config";
 
 import { defineConfig } from "drizzle-kit";
 
-if (!process.env.DB_URL) {
-  throw new Error("DB_URL is required");
-}
-
 export default defineConfig({
   schema: "./src/schema.ts",
   out: "./migrations",
   dialect: "postgresql",
-  dbCredentials: {
-    url: process.env.DB_URL,
-  },
 });
