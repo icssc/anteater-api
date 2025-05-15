75e97bfd17dc47bcea078f10ebd50475be942c8e
waterkimchi <hyunsul2@uci.edu>
2025-05-14 21:45:54 -0700

    Revert "feat: updated index for new dim"
    This reverts commit 8826df94e7a1cb6c41c2f39200c9c4803e974522.

--- packages/db/migrations/0002_search_indexes.sql
+++ packages/db/migrations/0002_search_indexes.sql
@@ -3,7 +3,6 @@ CREATE INDEX IF NOT EXISTS "course_search_index" ON "course" USING gin ((
  SETWEIGHT(TO_TSVECTOR('english', COALESCE("id", '')), 'A') ||
  SETWEIGHT(TO_TSVECTOR('english', COALESCE("department", '')), 'B') ||
  SETWEIGHT(TO_TSVECTOR('english', COALESCE("department_alias", '')), 'B') ||
- SETWEIGHT(TO_TSVECTOR('english', COALESCE("course.shortenedDept", '')), 'B') ||
  SETWEIGHT(TO_TSVECTOR('english', COALESCE("course_number", '')), 'B') ||
  SETWEIGHT(TO_TSVECTOR('english', COALESCE("course_numeric"::TEXT, '')), 'B') ||
  SETWEIGHT(TO_TSVECTOR('english', COALESCE("title", '')), 'C') ||
