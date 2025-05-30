687ee50733fcabfd22af2173c754f4dd4a9808ea
waterkimchi <hyunsul2@uci.edu>
2025-05-29 17:04:48 -0700

    fix: removed migrations

--- packages/db/migrations/0013_map_locations.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS "map_location" (
-	"id" numeric PRIMARY KEY NOT NULL,
-	"name" varchar NOT NULL,
-	"latitude" numeric NOT NULL,
-	"longitude" numeric NOT NULL,
-	"image_urls" json
-);
