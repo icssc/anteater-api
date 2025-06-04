1dc3868f556e3249a9577b963300bd0324b56482
waterkimchi <hyunsul2@uci.edu>
2025-06-03 20:04:51 -0700

    fix: edited scraper structure and schema

--- packages/db/migrations/0016_map_locations.sql
+++ packages/db/migrations/0016_map_locations.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS "map_location" (
-	"id" numeric PRIMARY KEY NOT NULL,
+	"id" varchar PRIMARY KEY NOT NULL,
 	"name" varchar NOT NULL,
 	"latitude" numeric NOT NULL,
-	"longitude" numeric NOT NULL,
-	"image_urls" json
+	"longitude" numeric NOT NULL
 );
