d54a6c4632a5c98d5abc24fbb635999da965d872
Dante Dam <laggycomputer@yahoo.com>
2025-08-02 16:51:03 -0700

    fix(search): resultType in graphql

--- apps/api/src/graphql/schema/search.ts
+++ apps/api/src/graphql/schema/search.ts
@@ -6,6 +6,11 @@ type SearchResult @cacheControl(maxAge: 86400) {
     rank: Float!
 }
 
+enum SearchResultType {
+    course,
+    instructor,
+}
+
 type SearchResponse {
     count: Int!
     results: [SearchResult!]!
@@ -15,6 +20,7 @@ input SearchQuery {
     query: String!
     take: Int
     skip: Int
+    resultType: SearchResultType
 }
 
 extend type Query {
