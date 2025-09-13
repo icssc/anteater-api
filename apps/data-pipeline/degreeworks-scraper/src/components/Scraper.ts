f91003cd94aa8e68b4b8a79c278920138ae73193
Dante Dam <laggycomputer@yahoo.com>
2025-09-12 21:28:06 -0700

    correct type cast when generating program triplets

--- apps/data-pipeline/degreeworks-scraper/src/components/Scraper.ts
+++ apps/data-pipeline/degreeworks-scraper/src/components/Scraper.ts
@@ -109,10 +109,11 @@ export class Scraper {
             new Date(`${ent.major.endTermYyyyst.slice(1)}-01-01`).getUTCFullYear() >= 2006) &&
           this.majorPrograms.has(ent.major.majorCode),
       )
-      .flatMap((ent) =>
-        this.findDwNameFor(awardTypesMap, ent)
-          .map((dwName) => [ent.school.schoolCode, ent.major.majorCode, dwName])
-          .toArray(),
+      .flatMap(
+        (ent) =>
+          this.findDwNameFor(awardTypesMap, ent)
+            .map((dwName) => [ent.school.schoolCode, ent.major.majorCode, dwName])
+            .toArray() as ProgramTriplet[],
       );
   }
 
