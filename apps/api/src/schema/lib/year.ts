db11666be1a22a6f97746a286b29b3168f8a9292
Hari Pathanjaly <haripathanjaly@gmail.com>
2025-10-05 13:32:48 -0700

    made year check in apps/api/schema/lib/year.ts explicit to handle empty string, null, and undefined

--- apps/api/src/schema/lib/year.ts
+++ apps/api/src/schema/lib/year.ts
@@ -6,10 +6,10 @@ import { z } from "@hono/zod-openapi";
 
 export const yearSchema = z.coerce
   .string()
-  .refine((val) => val !== ("" || "undefined" || "null"), {
+  .refine((val: string) => val !== "" && val !== "null" && val !== "undefined", {
     message: "Parameter 'year' is required",
   })
-  .refine((val) => /^\d{4}$/.test(val), {
+  .refine((val: string) => /^\d{4}$/.test(val), {
     message: "Year must be a 4-digit positive integer",
   })
   .openapi({ example: "2024" });
