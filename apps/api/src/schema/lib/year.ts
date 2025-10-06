5fbedd466219fdb966b09e8878aad45c61082172
Hari Pathanjaly <haripathanjaly@gmail.com>
2025-10-06 16:09:14 -0700

    removed type annotations on refine calls

--- apps/api/src/schema/lib/year.ts
+++ apps/api/src/schema/lib/year.ts
@@ -6,10 +6,10 @@ import { z } from "@hono/zod-openapi";
 
 export const yearSchema = z.coerce
   .string()
-  .refine((val: string) => val !== "null" && val !== "undefined", {
+  .refine((val) => val !== "null" && val !== "undefined", {
     message: "Parameter 'year' is required",
   })
-  .refine((val: string) => /^\d{4}$/.test(val), {
+  .refine((val) => /^\d{4}$/.test(val), {
     message: "Year must be a 4-digit positive integer",
   })
   .openapi({ example: "2024" });
