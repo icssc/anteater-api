b1fc38b733c91251f5d68a0999c6febd2d12cc64
Aadi <aadiscores13@gmail.com>
2025-10-15 10:52:50 -0700

    Revert "feat: Add migration entry for sample program (0019_sample_program)"
    This reverts commit e98e26fac917f269b5d769932d738427eb4b75bf.

--- packages/db/migrations/0019_sample_program.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS "sample_program" (
-	"id" varchar PRIMARY KEY NOT NULL,
-	"program_name" varchar NOT NULL,
-	"sample_program" json NOT NULL,
-	"program_notes" varchar[] NOT NULL
-);
