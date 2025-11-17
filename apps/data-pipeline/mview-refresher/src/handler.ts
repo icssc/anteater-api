b13b3f72ab4ffb94446f5eeebbc98afbaa150e0f
Hwijung <hwi.meh@gmail.com>
2025-11-16 22:12:30 -0800

    fix: refresh materialized view for courseView after running course scraper (#258)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    <!--- Describe your changes in detail -->
    refresh materialized view for courseView after running course scraper
    
    ## Related Issue
    
    <!--- This project only accepts pull requests related to open issues -->
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Please link to the issue here: -->
    
    #153 
    
    ## Motivation and Context
    
    <!--- Why is this change required? What problem does it solve? -->
    Updating materialized view after scrape prevents outdated data from
    being given.
    
    ## How Has This Been Tested?
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    Scraper was run locally.
    Whether the materialized view is refreshed has not been checked.
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] My code involves a change to the database schema.
    - [ ] My code requires a change to the documentation.

--- apps/data-pipeline/mview-refresher/src/handler.ts
+++ apps/data-pipeline/mview-refresher/src/handler.ts
@@ -1,10 +1,9 @@
 import { database } from "@packages/db";
-import { courseView, instructorView } from "@packages/db/schema";
+import { instructorView } from "@packages/db/schema";
 
 export default {
   async scheduled(_, env) {
     const db = database(env.DB.connectionString);
-    await db.refreshMaterializedView(courseView);
     await db.refreshMaterializedView(instructorView);
     await db.$client.end({ timeout: 5 });
   },
