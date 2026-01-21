6d2ab3a3de2d07e2697c6cb0a2914937c5ff3ab8
Andrew Wang <awwang3@uci.edu>
2026-01-21 09:42:31 -0800

    chore: update sst keys

--- apps/key-manager/sst.config.ts
+++ apps/key-manager/sst.config.ts
@@ -22,6 +22,7 @@ export default $config({
         AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
         AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
         AUTH_SECRET: process.env.AUTH_SECRET,
+        AUTH_TRUST_HOST: process.env.AUTH_TRUST_HOST,
       },
     });
   },
