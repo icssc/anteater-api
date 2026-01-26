a78daa12f2bad5f2be50970b368485f275342408
Dante Dam <laggycomputer@yahoo.com>
2026-01-25 19:57:58 -0800

    stray logs

--- apps/data-pipeline/dining-scraper/src/lib.ts
+++ apps/data-pipeline/dining-scraper/src/lib.ts
@@ -6,8 +6,8 @@ export async function doScrape(db: ReturnType<typeof database>) {
   await updateEvents(db);
 
   const today = new Date();
-  console.log("[weekly] Starting Brandywine and Anteatery Menu jobs...");
+  console.log("Starting Brandywine and Anteatery Menu jobs...");
   await updateRestaurant(db, today, "brandywine");
   await updateRestaurant(db, today, "anteatery");
-  console.log("[weekly] Finished Brandywine and Anteatery Menu jobs.");
+  console.log("Finished Brandywine and Anteatery Menu jobs.");
 }
