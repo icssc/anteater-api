83ac454f52f766e66d1f4a429d163e181a0f39e8
Dante Dam <laggycomputer@yahoo.com>
2026-01-25 20:23:04 -0800

    don't need all these headers
    whew

--- apps/data-pipeline/dining-scraper/src/fetch-event-images.ts
+++ apps/data-pipeline/dining-scraper/src/fetch-event-images.ts
@@ -25,25 +25,8 @@ export async function fetchEventImages(): Promise<Map<string, string>> {
     "https://uci.mydininghub.com/graphql/execute.json/elevate/events;root=/content/dam/cf/ch/uci/en/events/;campus=campus",
     {
       headers: {
-        Host: "uci.mydininghub.com",
-        "User-Agent": " Mozilla/5.0 (X11; Linux x86_64; rv:143.0) Gecko/20100101 Firefox/143.0",
-        Accept: "*/*",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate, br, zstd",
         Referer: "https://uci.mydininghub.com/en/locations/events",
-        newrelic:
-          "eyJ2IjpbMCwxXSwiZCI6eyJ0eSI6IkJyb3dzZXIiLCJhYyI6IjQ2MTE4MjQiLCJhcCI6IjExMDM0MDM4NjYiLCJpZCI6IjFjZTY1YzNiZWEzMmQ2YzEiLCJ0ciI6IjQwYmQwNjE2MzM2ZmMwZDQ5NGEyNDQ4OTQxYzM4NDExIiwidGkiOjE3NjMyNDIxOTg3Njd9fQ==",
-        traceparent: "00-40bd0616336fc0d494a2448941c38411-1ce65c3bea32d6c1-01",
-        tracestate: "4611824@nr=0-1-4611824-1103403866-1ce65c3bea32d6c1----1763242198767",
-        "x-api-key": "a1781e57-440f-4c70-9d65-89c8755dc5ad",
-        Connection: "keep-alive",
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "same-origin",
-        DNT: "1",
-        "Sec-GPC": "1",
-        Priority: "u=4",
-        TE: "trailers",
+        "X-API-Key": "a1781e57-440f-4c70-9d65-89c8755dc5ad",
       },
     },
   ).then((r) => r.json());
--- apps/data-pipeline/dining-scraper/src/query.ts
+++ apps/data-pipeline/dining-scraper/src/query.ts
@@ -9,8 +9,6 @@ export async function queryAdobeECommerce<Schema extends z.Schema>(
     `https://api.elevate-dxp.com/api/mesh/c087f756-cc72-4649-a36f-3a41b700c519/graphql?query=${encodeURIComponent(query)}&variables=${encodeURIComponent(JSON.stringify(variables))}`,
     {
       headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:141.0) Gecko/20100101 Firefox/141.0",
         Referer: "https://uci.mydininghub.com/",
         "content-type": "application/json",
         store: "ch_uci_en",
