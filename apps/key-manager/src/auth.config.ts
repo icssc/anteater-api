a95a60ac06fad01197013c453679d67e6d4858c7
Sanskar Mishra <69398106+sanskarm7@users.noreply.github.com>
2026-01-27 16:02:58 -0800

    Revert "ci: fix key manager" (#293)
    Reverts icssc/anteater-api#265

--- apps/key-manager/src/auth.config.ts
+++ apps/key-manager/src/auth.config.ts
@@ -1,12 +1,5 @@
 import Google from "next-auth/providers/google";
 
 import type { NextAuthConfig } from "next-auth";
-import { z } from "zod";
 
-const { AUTH_GOOGLE_ID, AUTH_GOOGLE_SECRET } = z
-  .object({ AUTH_GOOGLE_ID: z.string(), AUTH_GOOGLE_SECRET: z.string() })
-  .parse(process.env);
-
-export default {
-  providers: [Google({ clientId: AUTH_GOOGLE_ID, clientSecret: AUTH_GOOGLE_SECRET })],
-} satisfies NextAuthConfig;
+export default { providers: [Google] } satisfies NextAuthConfig;
