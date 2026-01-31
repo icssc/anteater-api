eedde20cf61fc8f3d6f3beccb8703d0506411632
Bill Liu <bill.liu.797@gmail.com>
2026-01-30 16:47:59 -0800

    chore: revise event rest schema openapi docs and start time nullability

--- apps/api/src/schema/dining.ts
+++ apps/api/src/schema/dining.ts
@@ -9,14 +9,14 @@ export const diningEventQuerySchema = z.object({
 
 export const eventSchema = z.object({
   title: z.string().openapi({ example: "Lunar New Year Celebration" }),
-  image: z.string().nullable().openapi({ example: "https://images.elevate-dxp.com/..." }),
-  restaurantId: z.string().openapi({ example: "3056" }),
+  image: z.string().nullable().openapi({ description: "URL to event promotional image" }),
+  restaurantId: z.string().openapi({
+    example: "3056",
+    description: "Optional unique identifier for desired restaurant",
+  }),
   longDescription: z.string().nullable(),
   // transforms Date objects in db to ISO 8601 strings (I hope)
-  start: z.coerce
-    .date()
-    .nullable()
-    .transform((d) => d?.toISOString() ?? null),
+  start: z.coerce.date().transform((d) => d?.toISOString()),
   end: z.coerce
     .date()
     .nullable()
