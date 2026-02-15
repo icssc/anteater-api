bd47eaaee965470b28376e165acdb85a141d1595
Hwijung Kim <hwi.meh@gmail.com>
2026-02-14 19:37:24 -0800

    made specRequirement a subtype of markerRequirement

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -124,9 +124,11 @@ export type DegreeWorksGroupRequirement = {
   requirements: DegreeWorksRequirement[];
 };
 
-export type DegreeWorksMarkerRequirement = {
-  requirementType: "Marker";
-};
+export type DegreeWorksMarkerRequirement =
+  | {
+      requirementType: "Marker";
+    }
+  | DegreeWorksSpecRequirement;
 
 export type DegreeWorksSpecRequirement = {
   requirementType: "Spec";
@@ -140,7 +142,6 @@ export type DegreeWorksRequirement = DegreeWorksRequirementBase &
     | DegreeWorksUnitRequirement
     | DegreeWorksGroupRequirement
     | DegreeWorksMarkerRequirement
-    | DegreeWorksSpecRequirement
   );
 
 export type APCoursesGrantedTree =
