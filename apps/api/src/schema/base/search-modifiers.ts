0249add70cb36fb7164de993b96d42f302d85ada
matt-franklin225 <mattfranklin225@gmail.com>
2026-02-16 13:46:10 -0800

    const issue removed from search-modifiers

--- apps/api/src/schema/base/search-modifiers.ts
+++ apps/api/src/schema/base/search-modifiers.ts
@@ -3,8 +3,8 @@ import { z } from "@hono/zod-openapi";
 export const takeBaseSchema = z.coerce
   .number()
   .lte(100, "Page size must be less than or equal to 100")
-  .default(100) as const;
+  .default(100);
 
-export const skipBaseSchema = z.coerce.number().default(0) as const;
+export const skipBaseSchema = z.coerce.number().default(0);
 
 export const cursorBaseSchema = z.string().optional();
