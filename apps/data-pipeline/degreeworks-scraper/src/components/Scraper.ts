517606ca575c79247b92d6ca1ff20f7c835397ef
Hwijung Kim <hwi.meh@gmail.com>
2026-02-22 23:17:53 -0800

    Revert "still push specs into programs"
    This reverts commit 063f3a2f568a52a9d387801979c886f78f8ee873.

--- apps/data-pipeline/degreeworks-scraper/src/components/Scraper.ts
+++ apps/data-pipeline/degreeworks-scraper/src/components/Scraper.ts
@@ -352,8 +352,6 @@ export class Scraper {
             `(majorCode = ${foundMajorAssured.code}, degree = ${foundMajorAssured.degreeType})`,
         );
 
-        foundMajorAssured.specs.push(specCode);
-
         foundMajorSpecPairs.push([
           [
             foundMajorAssured.degreeType?.startsWith("B") ? "U" : "G",
