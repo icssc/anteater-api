7a292e6156b11fba729e9582860b003e9ad332b8
Hwijung Kim <hwi.meh@gmail.com>
2026-02-24 12:36:51 -0800

    fixed typo

--- apps/data-pipeline/degreeworks-scraper/src/components/Scraper.ts
+++ apps/data-pipeline/degreeworks-scraper/src/components/Scraper.ts
@@ -311,7 +311,7 @@ export class Scraper {
             foundMajor = majorProgram[1];
           } else {
             console.log(
-              `warning: ${specName} has cached relation to non-existant major, ${got.parent.name} (spec code = ${specCode})`,
+              `warning: ${specName} has cached relation to non-existent major, ${got.parent.name} (spec code = ${specCode})`,
             );
           }
         }
