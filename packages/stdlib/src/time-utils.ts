bdd92cb5818138a64bd8827c465eb34a558f6a5a
matt-franklin225 <mattfranklin225@gmail.com>
2026-03-05 14:42:37 -0800

    detailed examples added for time-utils.ts

--- packages/stdlib/src/time-utils.ts
+++ packages/stdlib/src/time-utils.ts
@@ -1,7 +1,10 @@
 /**
  * Converts a start-end time string to a number of minutes since midnight.
- *
  * @example parseStartAndEndTimes("11:00-11:50a"); // { startTime: 660, endTime: 710 }
+ * @example parseStartAndEndTimes("1:00-1:50p"); // { startTime: 780, endTime: 830 }
+ *
+ * Note that if the time passes through 12:00, it's assumed that it passed noon instead of midnight.
+ * @example parseStartAndEndTimes("11:00-12:50"); // { startTime: 660, endTime: 770 }
  *
  * @param time start-end time string
  * @returns startTime and endTime in minutes since midnight
