cc1279d61698a27ec7669badce7ccf56ce36ff7c
matt-franklin225 <mattfranklin225@gmail.com>
2026-03-10 12:19:03 -0700

    fixed breaking in time-utils

--- packages/stdlib/src/time-utils.ts
+++ packages/stdlib/src/time-utils.ts
@@ -2,11 +2,11 @@
  * Converts a start-end time string to a number of minutes since midnight.
  * If an end time doesn't specify "p" or "pm", we assume it ends in "am" based on WebSoc logic
  * @example parseStartAndEndTimes("11:00-11:50"); // { startTime: 660, endTime: 710 }
- * @example parseStartAndEndTimes("1:00-1:50p"); // { startTime: 780, endTime: 830 }
+ * @example parseStartAndEndTimes("12:00-12:50p"); // { startTime: 720, endTime: 770 }
  * @example parseStartAndEndTimes("11:00-2:50p"); // { startTime: 660, endTime: 890 }
  *
  * If a time passes through midnight, we return number of minutes passed since the midnight of the first day.
- * @example parseStartAndEndTimes("11:00-1:50"); // { startTime: 660, endTime: 1550 }
+ * @example parseStartAndEndTimes("11:00-1:50"); // { startTime: 1380, endTime: 1550 }
  *
  * @param time start-end time string
  * @returns startTime and endTime in minutes since midnight
@@ -23,6 +23,10 @@ export const parseStartAndEndTimes = (time: string) => {
   const [endTimeHour, endTimeMinute] = endTimeString.split(":");
   endTime = (Number.parseInt(endTimeHour, 10) % 12) * 60 + Number.parseInt(endTimeMinute, 10);
   if (endTimeMinute.includes("p") && startTime <= endTime) startTime += 12 * 60;
-  if (startTime > endTime) endTime += 24 * 60;
+  if (endTimeMinute.includes("p") && startTime > endTime) endTime += 12 * 60;
+  if (!endTimeMinute.includes("p") && startTime > endTime) {
+    startTime += 12 * 60;
+    endTime += 24 * 60;
+  }
   return { startTime, endTime };
 };
