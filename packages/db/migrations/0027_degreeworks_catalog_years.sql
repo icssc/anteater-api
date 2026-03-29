5f100827453b9e04b6b19e40cfd74ebb819e390f
Dante Dam <d@dantedam.com>
2026-03-28 22:46:35 -0700

    sigh

--- packages/db/migrations/0027_degreeworks_catalog_years.sql
+++ packages/db/migrations/0027_degreeworks_catalog_years.sql
@@ -34,28 +34,12 @@ ALTER TABLE "dw_specialization" DROP CONSTRAINT "specialization_major_id_major_i
 DROP INDEX IF EXISTS "major_degree_id_index";--> statement-breakpoint
 DROP INDEX IF EXISTS "major_college_requirement_index";--> statement-breakpoint
 DROP INDEX IF EXISTS "specialization_major_id_index";--> statement-breakpoint
-ALTER TABLE "dw_college_requirement" ALTER COLUMN "id" SET DATA TYPE bigint;--> statement-breakpoint
-ALTER TABLE "dw_college_requirement" ALTER COLUMN "id" DROP DEFAULT;--> statement-breakpoint
 ALTER TABLE "dw_college_requirement" drop column "id";--> statement-breakpoint
 ALTER TABLE "dw_college_requirement" ADD COLUMN "id" bigint PRIMARY KEY GENERATED ALWAYS AS (('x' || substr(md5(name), 1, 16))::bit(64)::bigint # jsonb_hash_extended(requirements, 0)) STORED NOT NULL;--> statement-breakpoint
-/* 
-    Unfortunately in current drizzle-kit version we can't automatically get name for primary key.
-    We are working on making it available!
-
-    Meanwhile you can:
-        1. Check pk name in your database, by running
-            SELECT constraint_name FROM information_schema.table_constraints
-            WHERE table_schema = 'public'
-                AND table_name = 'dw_school'
-                AND constraint_type = 'PRIMARY KEY';
-        2. Uncomment code below and paste pk name manually
-        
-    Hope to release this update as soon as possible
-*/
-
--- ALTER TABLE "dw_school" DROP CONSTRAINT "<constraint_name>";--> statement-breakpoint
+ALTER TABLE "dw_school" DROP CONSTRAINT "school_requirement_pkey";--> statement-breakpoint
 ALTER TABLE "dw_school" ALTER COLUMN "requirements" SET DATA TYPE jsonb;--> statement-breakpoint
 ALTER TABLE "dw_school" ADD COLUMN "catalog_year" varchar NOT NULL;--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS "dw_school_id_catalog_year_index" ON "dw_school" USING btree ("id","catalog_year");--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "dw_major_requirement" ADD CONSTRAINT "dw_major_requirement_program_id_dw_major_id_fk" FOREIGN KEY ("program_id") REFERENCES "public"."dw_major"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
