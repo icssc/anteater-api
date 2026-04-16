412cd4381a69452f1a4ab6a6b579caeca6da34ce
Hwijung Kim <hwi.meh@gmail.com>
2026-04-16 11:26:58 -0700

    actually remigrated

---
+++ packages/db/migrations/0029_polymorhpic_major_requirements.sql
@@ -0,0 +1,16 @@
+git CREATE TABLE "major_requirement" (
+	"requirements" jsonb NOT NULL,
+	"id" bigint PRIMARY KEY GENERATED ALWAYS AS (jsonb_hash_extended(requirements, 0)) STORED NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "major_specialization_to_requirement" (
+	"major_id" varchar NOT NULL,
+	"specialization_id" varchar,
+	"requirement_id" bigint NOT NULL,
+	CONSTRAINT "major_specialization_to_requirement_major_id_specialization_id_unique" UNIQUE NULLS NOT DISTINCT("major_id","specialization_id")
+);
+--> statement-breakpoint
+ALTER TABLE "major_specialization_to_requirement" ADD CONSTRAINT "major_specialization_to_requirement_major_id_major_id_fk" FOREIGN KEY ("major_id") REFERENCES "public"."major"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "major_specialization_to_requirement" ADD CONSTRAINT "major_specialization_to_requirement_specialization_id_specialization_id_fk" FOREIGN KEY ("specialization_id") REFERENCES "public"."specialization"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "major_specialization_to_requirement" ADD CONSTRAINT "major_specialization_to_requirement_requirement_id_major_requirement_id_fk" FOREIGN KEY ("requirement_id") REFERENCES "public"."major_requirement"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "major" DROP COLUMN "requirements";
