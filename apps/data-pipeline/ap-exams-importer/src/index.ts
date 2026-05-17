82d2e07cc68b1de2b994223d2d24cab013cf0661
Bill Liu <122062820+iAmbrosial@users.noreply.github.com>
2026-05-16 18:59:23 -0700

    fix: ap exam importer should respect existing data (#406)
    ## Description
    
    Use new ap exam rewards data while removing stale junction rows.
    
    ## Related Issue
    
    #164 
    
    ## Motivation and Context
    
    Make data good
    
    ## How Has This Been Tested?
    
    Ran ap exams importer
    
    ## Screenshots (if appropriate):
    
    ## Types of changes
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    ## Checklist:
    
    - [ ] My code involves a change to the database schema.
    - [ ] My code requires a change to the documentation.

--- apps/data-pipeline/ap-exams-importer/src/index.ts
+++ apps/data-pipeline/ap-exams-importer/src/index.ts
@@ -1,7 +1,7 @@
 import { exit } from "node:process";
 
 import { database } from "@packages/db";
-
+import { notInArray } from "@packages/db/drizzle";
 import { apExam, apExamReward, apExamToReward } from "@packages/db/schema";
 import { conflictUpdateSetAllCols } from "@packages/db/utils";
 import apExamData, { geCategories, type geCategory } from "./data.ts";
@@ -50,10 +50,22 @@ async function main() {
           .then((rows) => rows[0]);
 
         for (const score of reward.acceptableScores) {
-          await tx.insert(apExamToReward).values({ examId: fullName, score, reward: rewardId });
+          await tx
+            .insert(apExamToReward)
+            .values({ examId: fullName, score, reward: rewardId })
+            .onConflictDoUpdate({
+              target: [apExamToReward.examId, apExamToReward.score],
+              set: conflictUpdateSetAllCols(apExamToReward),
+            });
         }
       }
     }
+
+    await tx
+      .delete(apExamReward)
+      .where(
+        notInArray(apExamReward.id, tx.select({ id: apExamToReward.reward }).from(apExamToReward)),
+      );
   });
 
   await db.$client.end();
