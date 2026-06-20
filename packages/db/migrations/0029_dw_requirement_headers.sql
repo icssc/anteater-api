24c544255b92d1eba8f97813404aa45aa8446a39
Hwijung Kim <hwi.meh@gmail.com>
2026-06-20 14:02:21 -0700

    removed mig for merge

--- packages/db/migrations/0029_dw_requirement_headers.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "college_requirement" ADD COLUMN "header" json;--> statement-breakpoint
-ALTER TABLE "major" ADD COLUMN "header" json;--> statement-breakpoint
-ALTER TABLE "minor" ADD COLUMN "header" json;--> statement-breakpoint
-ALTER TABLE "school_requirement" ADD COLUMN "header" json;--> statement-breakpoint
-ALTER TABLE "specialization" ADD COLUMN "header" json;
