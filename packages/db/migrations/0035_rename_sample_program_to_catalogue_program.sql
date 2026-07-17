7dff2df5701cb89e1a1a87ed66ccf61ca3dbb46e
Leon Wang <leonwang1016@gmail.com>
2026-07-16 22:57:24 -0700

    fix: use drizzle generate for migration and revert unintended changes

--- packages/db/migrations/0035_rename_sample_program_to_catalogue_program.sql
+++ packages/db/migrations/0035_rename_sample_program_to_catalogue_program.sql
@@ -1,3 +1,5 @@
-ALTER TABLE "sample_program_variation" RENAME TO "catalogue_program_variation";
-ALTER TABLE "catalogue_program_variation" RENAME COLUMN "sample_program" TO "catalogue_program";
-ALTER TABLE "catalogue_program_variation" RENAME CONSTRAINT "sample_program_variation_program_id_catalogue_program_id_fk" TO "catalogue_program_variation_program_id_catalogue_program_id_fk";
+ALTER TABLE "sample_program_variation" RENAME TO "catalogue_program_variation";--> statement-breakpoint
+ALTER TABLE "catalogue_program_variation" RENAME COLUMN "sample_program" TO "catalogue_program";--> statement-breakpoint
+ALTER TABLE "catalogue_program_variation" DROP CONSTRAINT "sample_program_variation_program_id_catalogue_program_id_fk";
+--> statement-breakpoint
+ALTER TABLE "catalogue_program_variation" ADD CONSTRAINT "catalogue_program_variation_program_id_catalogue_program_id_fk" FOREIGN KEY ("program_id") REFERENCES "public"."catalogue_program"("id") ON DELETE cascade ON UPDATE no action;
