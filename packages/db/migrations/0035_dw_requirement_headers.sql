387aaf70db303603106319194695b0a7326eee88
Hwijung Kim <hwi.meh@gmail.com>
2026-07-26 12:03:07 -0700

    remove mig

--- packages/db/migrations/0035_dw_requirement_headers.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "dw_major_requirement" ADD COLUMN "header" jsonb;--> statement-breakpoint
-ALTER TABLE "dw_major_year" ADD COLUMN "college_header" jsonb;--> statement-breakpoint
-ALTER TABLE "dw_minor_requirement" ADD COLUMN "header" jsonb;--> statement-breakpoint
-ALTER TABLE "dw_school_requirement" ADD COLUMN "header" jsonb;--> statement-breakpoint
-ALTER TABLE "dw_specialization_requirement" ADD COLUMN "header" jsonb;
