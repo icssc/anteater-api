394fc7e34f670afffe3efc71bb3d3c464b028d61
Dante Dam <d@dantedam.com>
2026-08-12 13:21:41 -0700

    comments

--- apps/api/src/middleware/key-verifier.ts
+++ apps/api/src/middleware/key-verifier.ts
@@ -11,8 +11,12 @@ export const keyVerifier = createMiddleware<{ Bindings: Env }>(async (c, next) =
       throw new HTTPException(401, {
         message: `Authorization scheme '${scheme}' is not supported`,
       });
+
+    // we can skip conversion if type checking statically verifies we don't need to
+    // if there were multiple formats in the future, we could match on the discriminator here
     const keyData = await c.env.API_KEYS.get<KeyInStorage["value"]>(key, { type: "json" });
     if (!keyData) throw new HTTPException(401, { message: "Invalid or expired API key" });
+
     if (keyData._type === "publishable") {
       if (!origin) throw new HTTPException(401, { message: "Origin not provided" });
       if (!keyData.origins[origin]) throw new HTTPException(401, { message: "Invalid origin" });
