45e0f44ce1b97eac173d9f61482b9e01a5660b00
Hwijung Kim <hwi.meh@gmail.com>
2026-08-13 10:45:54 -0700

    oopsie 2

--- apps/api/src/schema/programs.ts
+++ apps/api/src/schema/programs.ts
@@ -114,7 +114,7 @@ export const nonExclusiveQualifierSchema = z
           .optional()
           .openapi({
             description:
-              "The code of a specific program this qualifier applies to. These are ids for majors, minors, and specializations (i.e. `BS-201`, `120`, `BS-201A`); numerical codes representing colleges for college requirements (i.e 55 for School of Biological Sciences); and misc strings for other blocks (i.e 'LIBL' for Liberal Learning). If no code is specified, this qualifier applies to all programs of the specified `programType`",
+              "The code of a specific program this qualifier applies to. These are ids for majors, minors, and specializations (i.e. `BS-201`, `120`, `BS-201A`); numerical codes representing colleges for college requirements (i.e `55` for School of Biological Sciences); and misc strings for other blocks (i.e `LIBL` for Liberal Learning). If no code is specified, this qualifier applies to all programs of the specified `programType`",
             examples: ["BS-201", "120", "BS-201A", "55", "LIBL"],
           }),
       }),
